d55f45afa5781cdda725e9d857e5adb3da2588ee
Nelson Gong <nelsongong642006@gmail.com>
2022-11-10 21:01:59 +0000

    i must have pogor

--- Scripts/Cannon.gd
+++ Scripts/Cannon.gd
@@ -2,12 +2,6 @@ extends Area2D
 
 export var strength : float = 1
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
